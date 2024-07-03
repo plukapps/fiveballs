fb20b82ed6021033a2338a84266e0081e595155d
santilod <santilod@gmail.com>
2024-07-03 17:50:47 -0300

    Add remote config for country flags

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -174,4 +174,6 @@ dependencies {
     // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-crashlytics")
+    implementation("com.google.firebase:firebase-config")
+
 }
--- app/src/main/java/com/pluk/fiveballs/view/FiveMoreApp.java
+++ app/src/main/java/com/pluk/fiveballs/view/FiveMoreApp.java
@@ -4,6 +4,9 @@ import android.app.Application;
 
 import com.google.android.gms.ads.MobileAds;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+import com.pluk.fiveballs.R;
 
 public class FiveMoreApp extends Application {
 
@@ -11,13 +14,44 @@ public class FiveMoreApp extends Application {
     public void onCreate() {
         super.onCreate();
 
-        FirebaseApp.initializeApp(this);
+        configFirebase();
+        configAds();
+        configRemoteConfig();
+    }
 
+    private void configFirebase() {
+        FirebaseApp.initializeApp(this);
+    }
+
+    private void configAds() {
         new Thread(
                 () -> {
                     // Initialize the Google Mobile Ads SDK on a background thread.
                     MobileAds.initialize(this, initializationStatus -> {});
                 })
                 .start();
+    }
+
+    private void configRemoteConfig() {
+        FirebaseRemoteConfig remoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setMinimumFetchIntervalInSeconds(60)
+                .build();
+        remoteConfig.setConfigSettingsAsync(configSettings);
+
+        remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults);
+
+//        remoteConfig.addOnConfigUpdateListener(new ConfigUpdateListener() {
+//            @Override
+//            public void onUpdate(@NonNull ConfigUpdate configUpdate) {
+//                Log.d("FiveMoreApp", "Updated keys: " + configUpdate.getUpdatedKeys());
+//                remoteConfig.activate();
+//            }
+//
+//            @Override
+//            public void onError(@NonNull FirebaseRemoteConfigException error) {
+//                Log.d("FiveMoreApp", "Updated fail:" + error);
+//            }
+//        });
     }
 }
